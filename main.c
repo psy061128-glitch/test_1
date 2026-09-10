9dc84ecf2a72f713c11042f5831301404109b623
psy061128-glitch <psy061128@yonsei.ac.kr>
2026-09-10 18:18:11 +0900

    Add main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int input_num = 0;
+
+    scanf("%d", &input_num);
+
+    printf("%d\n", input_num);
+
+    return 0;
+}
